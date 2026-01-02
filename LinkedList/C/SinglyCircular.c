69f1cb8890049861aafe12af55f60ea2c90f49d7
aditya8242 <aditya.h.chavan@gmail.com>
2026-01-03 00:04:51 +0530

    added linked list implementation in C

---
+++ LinkedList/C/SinglyCircular.c
@@ -0,0 +1,358 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, PPNODE last, int no)
+{
+	// node creation
+	PNODE newn = NULL;
+	newn = (PNODE)malloc(sizeof(NODE));
+	
+	// node initialization
+	newn->data = no;
+	newn->next = NULL;
+
+	// if LL is empty
+	if(NULL == *first && NULL == *last)
+	{
+		*first = newn;
+		*last = newn;
+	}
+	else
+	{
+		newn->next = *first;
+		*first = newn;
+	}
+
+	(*last)->next = *first;	// important to maintain circular nature
+}
+
+void InsertLast(PPNODE first, PPNODE last, int no)
+{
+	// node creation
+	PNODE newn = NULL;
+	newn = (PNODE)malloc(sizeof(NODE));
+	
+	// node initialization
+	newn->data = no;
+	newn->next = NULL;
+
+	// if LL is empty
+	if(NULL == *first && NULL == *last)
+	{
+		*first = newn;
+		*last = newn;
+	}
+	else
+	{
+		(*last)->next = newn;
+		*last = newn;
+	}
+
+	(*last)->next = *first;
+}
+
+void DeleteFirst(PPNODE first, PPNODE last)
+{
+	// temporary pointer
+	PNODE temp = NULL;
+
+	if(NULL == *first && NULL == *last)	// no elements
+	{
+		return;
+	}
+	else if(*first == *last)	// single element
+	{
+		free(*first);
+
+		*first = NULL;
+		*last = NULL;
+	}
+	else    // if more than one elements
+	{
+		temp = *first;
+		*first = (*first)->next;
+		free(temp);
+
+		(*last)->next = *first;
+	}
+}
+
+void DeleteLast(PPNODE first, PPNODE last)
+{
+	// temporary pointer
+	PNODE temp = NULL;
+
+	if(NULL == *first && NULL == *last)
+	{
+		return;
+	}
+	else if(*first == *last)
+	{
+		free(*first);
+
+		*first = NULL;
+		*last = NULL;
+	}
+	else
+	{
+		temp = *first;
+
+		while(temp->next != *last)
+		{
+			temp = temp->next;
+		}
+
+		free(*last);
+		*last = temp;
+
+		(*last)->next = *first;
+	}
+}
+
+void Display(PNODE first, PNODE last)
+{
+	do
+	{
+		printf("| %d |->", first->data);
+		first = first->next;
+	}while(first != last->next);
+}
+
+int Count(PNODE first, PNODE last)
+{
+	int iCount = 0;
+	do
+	{
+		iCount++;
+		first = first->next;
+	}while(first != last->next);
+
+	return iCount;
+}
+
+void InsertAtPos(PPNODE first, PPNODE last, int no, int pos)
+{
+	int iSize = Count(*first, *last);
+
+	PNODE temp = NULL;
+	PNODE newn = NULL;
+
+	int iCnt = 0;
+
+	if(pos < 1 || pos > iSize+1)
+	{
+		printf("Invalid position.\n");
+		return;
+	}
+
+	if(pos == 1)
+	{
+		InsertFirst(first, last, no);
+	}
+	else if(pos == iSize+1)
+	{
+		InsertLast(first, last, no);
+	}
+	else
+	{
+		temp = *first;
+
+		newn = (PNODE)malloc(sizeof(NODE));
+		newn->data = no;
+		newn->next = NULL;
+
+		for(iCnt = 1; iCnt < pos - 1; iCnt++)
+		{
+			temp = temp->next;
+		}
+
+		newn->next = temp->next;
+		temp->next = newn;
+	}
+
+}
+
+void DeleteAtPos(PPNODE first, PPNODE last, int pos)
+{
+	int iSize = 0;
+	
+	iSize = Count(*first, *last);
+
+	PNODE temp = NULL;
+	PNODE target = NULL;
+
+	int iCnt = 0;
+
+	if(pos < 1 || pos > iSize)
+	{
+		printf("Invalid position.\n");
+		return;
+	}
+
+	if(pos == 1)
+	{
+		DeleteFirst(first, last);
+	}
+	else if(pos == iSize)
+	{
+		DeleteLast(first, last);
+	}
+	else
+	{
+		temp = *first;
+
+		for(iCnt = 1; iCnt < pos - 1; iCnt++)
+		{
+			temp = temp->next;
+		}
+
+		target = temp->next;
+
+		temp->next = target->next;
+		free(target);
+	}	
+}
+
+int main()
+{
+	PNODE head = NULL;
+	PNODE tail = NULL;
+
+	int iChoice = 0;
+	int iData = 0;
+	int iRet = 0;
+	int iFlag = 0;
+	int iPos = 0;
+
+	printf("%50s=====", "=====Singly Circular Linked List");
+	while(true)
+	{
+		printf("\n\nOperations:\n1.Insert First\n2.Insert Last\n3.Insert at position\n");
+		printf("4.Delete First\n5.Delete Last\n6.Delete at position\n");
+		printf("7.Count\n8.Display\n9.Exit\n");
+
+		printf("Enter your choice: ");
+		iFlag = scanf("%d", &iChoice);	// scanf returns 1 if successful
+
+		if(iFlag == 1)
+		{
+			switch(iChoice)
+			{
+				case 1:
+					printf("\nEnter data: ");
+					scanf("%d", &iData);
+					InsertFirst(&head, &tail, iData);
+					printf("\nData inserted.");
+					break;
+
+				case 2:
+					printf("\nEnter data: ");
+					scanf("%d", &iData);
+					InsertLast(&head, &tail, iData);
+					printf("\nData inserted.");
+					break;
+				
+				case 3:
+					printf("\nEnter data: ");
+					scanf("%d", &iData);
+					printf("\nEnter position: ");
+					scanf("%d", &iPos);
+					InsertAtPos(&head, &tail, iData, iPos);
+					printf("\nData inserted.");
+					break;
+				
+				case 4:
+					if(NULL == head && NULL == tail)
+					{
+						printf("Linked List is empty.\n");
+					}
+					else
+					{
+						DeleteFirst(&head, &tail);
+						printf("Element deleted!");
+					}
+					break;
+
+				case 5:
+					if(NULL == head && NULL == tail)
+					{
+						printf("Linked List is empty.\n");
+					}
+					else
+					{
+						DeleteLast(&head, &tail);
+						printf("Element deleted!");
+					}
+					break;
+
+				case 6:
+					if(NULL == head && NULL == tail)
+					{
+						printf("Linked List is empty!\n");
+					}
+					else
+					{
+						printf("Enter position: ");
+						scanf("%d",&iPos);
+						DeleteAtPos(&head, &tail, iPos);
+						printf("Element deleted!\n");
+					}
+					break;
+
+				case 7:
+					iRet = Count(head, tail);
+					if(iRet == 0)
+					{
+						printf("Linked list is empty\n");
+					}
+					else
+					{
+						printf("Number of nodes are %d", iRet);
+					}
+					break;
+					
+				case 8:
+					if(NULL == head && NULL == tail)
+					{
+						printf("Linked is empty!\n");
+					}
+					else
+					{
+						Display(head, tail);
+					}
+					break;
+
+				case 9:
+					printf("Exiting program...\n");
+					exit(0);
+				
+				default:
+					printf("Enter valid choice!\n");
+					break;
+			}
+		}
+		else
+		{
+			printf("Invalid input!\nPlease enter integer.");
+
+			while(getchar() != '\n');	// getchar() takes other input
+		}								// and clears the input
+	}
+
+	printf("\n");
+
+	return 0;
+}
---
+++ LinkedList/C/SinglyLinear.c
@@ -0,0 +1,354 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#pragma pack(1)
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+	// node creation
+	PNODE newn = NULL;
+	newn = (PNODE)malloc(sizeof(NODE));
+	
+	// node initialization
+	newn->data = no;
+	newn->next = NULL;
+
+	// if LL is empty
+	if(NULL == *first)
+	{
+		*first = newn;
+	}
+	else
+	{
+		newn->next = *first;
+		*first = newn;
+	}
+}
+
+void InsertLast(PPNODE first, int no)
+{
+	// node creation
+	PNODE newn = NULL;
+	newn = (PNODE)malloc(sizeof(NODE));
+	
+	// node initialization
+	newn->data = no;
+	newn->next = NULL;
+
+	// temporary pointer to not mess with head
+	PNODE temp = NULL;
+
+	// if LL is empty
+	if(NULL == *first)
+	{
+		*first = newn;
+	}
+	else
+	{
+		temp = *first;
+
+		while(temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+
+		temp->next = newn;
+	}
+}
+
+void DeleteFirst(PPNODE first)
+{
+	// temporary pointer
+	PNODE temp = NULL;
+
+	if(NULL == *first)	// no elements
+	{
+		return;
+	}
+	else if((*first)->next == NULL)	// single element
+	{
+		free(*first);
+		*first = NULL;
+	}
+	else    // if more than one elements
+	{
+		temp = *first;
+		*first = (*first)->next;
+		free(temp);
+	}
+}
+
+void DeleteLast(PPNODE first)
+{
+	// temporary pointer
+	PNODE temp = NULL;
+
+	if(NULL == *first)
+	{
+		return;
+	}
+	else if((*first)->next == NULL)
+	{
+		free(*first);
+		*first = NULL;
+	}
+	else
+	{
+		temp = *first;
+
+		while(temp->next->next != NULL)
+		{
+			temp = temp->next;
+		}
+
+		free(temp->next);
+		temp->next = NULL;
+	}
+}
+
+void Display(PNODE first)
+{
+	printf("Linked Listed elements:\n");
+	while(first != NULL)
+	{
+		printf("| %d |->", first->data);
+		first = first->next;
+	}
+
+	printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+	int iCount = 0;
+	while(first != NULL)
+	{
+		iCount++;
+		first = first->next;
+	}
+
+	return iCount;
+}
+
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+	int iSize = Count(*first);
+
+	PNODE temp = NULL;
+	PNODE newn = NULL;
+
+	int iCnt = 0;
+
+	if(pos < 1 || pos > iSize+1)
+	{
+		printf("Invalid position.\n");
+		return;
+	}
+
+	if(pos == 1)
+	{
+		InsertFirst(first, no);
+	}
+	else if(pos == iSize+1)
+	{
+		InsertLast(first, no);
+	}
+	else
+	{
+		temp = *first;
+
+		newn = (PNODE)malloc(sizeof(NODE));
+		newn->data = no;
+		newn->next = NULL;
+
+		for(iCnt = 1; iCnt < pos - 1; iCnt++)
+		{
+			temp = temp->next;
+		}
+
+		newn->next = temp->next;
+		temp->next = newn;
+	}
+
+}
+
+void DeleteAtPos(PPNODE first, int pos)
+{
+	int iSize = 0;
+	
+	iSize = Count(*first);
+
+	PNODE temp = NULL;
+	PNODE target = NULL;
+
+	int iCnt = 0;
+
+	if(pos < 1 || pos > iSize)
+	{
+		printf("Invalid position.\n");
+		return;
+	}
+
+	if(pos == 1)
+	{
+		DeleteFirst(first);
+	}
+	else if(pos == iSize)
+	{
+		DeleteLast(first);
+	}
+	else
+	{
+		temp = *first;
+
+		for(iCnt = 1; iCnt < pos - 1; iCnt++)
+		{
+			temp = temp->next;
+		}
+
+		target = temp->next;
+
+		temp->next = target->next;
+		free(target);
+	}	
+}
+
+int main()
+{
+	PNODE head = NULL;
+	int iChoice = 0;
+	int iData = 0;
+	int iRet = 0;
+	int iFlag = 0;
+	int iPos = 0;
+
+	printf("%50s=====", "=====Singly Linear Linked List");
+	while(true)
+	{
+		printf("\n\nOperations:\n1.Insert First\n2.Insert Last\n3.Insert at position\n");
+		printf("4.Delete First\n5.Delete Last\n6.Delete at position\n");
+		printf("7.Count\n8.Display\n9.Exit\n");
+
+		printf("Enter your choice: ");
+		iFlag = scanf("%d", &iChoice);	// scanf returns 1 if successful
+
+		if(iFlag == 1)
+		{
+			switch(iChoice)
+			{
+				case 1:
+					printf("\nEnter data: ");
+					scanf("%d", &iData);
+					InsertFirst(&head, iData);
+					printf("\nData inserted.");
+					break;
+
+				case 2:
+					printf("\nEnter data: ");
+					scanf("%d", &iData);
+					InsertLast(&head, iData);
+					printf("\nData inserted.");
+					break;
+				
+				case 3:
+					printf("\nEnter data: ");
+					scanf("%d", &iData);
+					printf("\nEnter position: ");
+					scanf("%d", &iPos);
+					InsertAtPos(&head, iData, iPos);
+					printf("\nData inserted.");
+					break;
+				
+				case 4:
+					if(NULL == head)
+					{
+						printf("Linked List is empty.\n");
+					}
+					else
+					{
+						DeleteFirst(&head);
+						printf("Element deleted!");
+					}
+					break;
+
+				case 5:
+					if(NULL == head)
+					{
+						printf("Linked List is empty.\n");
+					}
+					else
+					{
+						DeleteLast(&head);
+						printf("Element deleted!");
+					}
+					break;
+
+				case 6:
+					if(NULL == head)
+					{
+						printf("Linked List is empty!\n");
+					}
+					else
+					{
+						printf("Enter position: ");
+						scanf("%d",&iPos);
+						DeleteAtPos(&head, iPos);
+						printf("Element deleted!\n");
+					}
+					break;
+
+				case 7:
+					iRet = Count(head);
+					if(iRet == 0)
+					{
+						printf("Linked list is empty\n");
+					}
+					else
+					{
+						printf("Number of nodes are %d", iRet);
+					}
+					break;
+					
+				case 8:
+					if(NULL == head)
+					{
+						printf("Linked is empty!\n");
+					}
+					else
+					{
+						Display(head);
+					}
+					break;
+
+				case 9:
+					printf("Exiting program...\n");
+					exit(0);
+				
+				default:
+					printf("Enter valid choice!\n");
+					break;
+			}
+		}
+		else
+		{
+			printf("Invalid input!\nPlease enter integer.");
+
+			while(getchar() != '\n');	// getchar() takes other input
+		}								// and clears the input
+	}
+
+	printf("\n");
+
+	return 0;
+}
